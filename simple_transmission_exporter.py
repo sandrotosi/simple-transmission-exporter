a1a63321f25129360d29dce57aecdafe385c8e28
Sandro Tosi <morph@debian.org>
2020-10-22 23:41:12 -0400

    Obtain detailed counters for torrents statuses

--- simple_transmission_exporter.py
+++ simple_transmission_exporter.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 import datetime
+import json
 import os
 import sys
+from collections import Counter
 
-from flask import Flask, make_response, request
 import transmissionrpc
-
+from flask import Flask, make_response, request
 
 METRIC_PREFIX = 'transmission'
 PORT = 29091
+# RPC direct query to obtain only torrents status
+TORRENTS_QUERY = json.dumps({"method": "torrent-get", "arguments": {"fields": ["status"]}})
+# the possible states a torrent can have
+# source: https://github.com/transmission/transmission/blob/master/libtransmission/transmission.h#L1649-L1659
+STATUS = {
+    0: 'paused',
+    1: 'queued_to_check',
+    2: 'checking',
+    3: 'queued_to_download',
+    4: 'downloading',
+    5: 'queued_to_seed',
+    6: 'seeding',
+}
 
 # verify all the environment variables are set
 for envvar in ["TRANSMISSION_HOST", "TRANSMISSION_PORT", "TRANSMISSION_USERNAME", "TRANSMISSION_PASSWORD"]:
@@ -38,13 +52,12 @@ metric page: {request.host_url}metrics
 
 @app.route('/metrics')
 def metrics():
-
     _return = []
     start = datetime.datetime.now()
     tc = transmissionrpc.Client(address=TRANSMISSION_HOST, port=TRANSMISSION_PORT, user=TRANSMISSION_USERNAME, password=TRANSMISSION_PASSWORD)
     stats = tc.session_stats()
 
-    for metric in ['activeTorrentCount', 'downloadSpeed', 'download_dir_free_space', 'pausedTorrentCount', 'torrentCount', 'uploadSpeed']:
+    for metric in ['downloadSpeed', 'download_dir_free_space', 'uploadSpeed']:
         _metric_name = f'{METRIC_PREFIX}_{metric}'
         _return.append((f'# TYPE {_metric_name}', 'gauge'))
         _return.append((_metric_name, stats._fields[metric].value))
@@ -55,10 +68,22 @@ def metrics():
             _return.append((f'# TYPE {_metric_name}', 'counter'))
             _return.append((_metric_name, stats._fields[metric].value[item]))
 
+    # obtain torrents status counters
+    # we need to query directly the RPC endpoint (still using transmissionrpc module for actually running the query)
+    # as `get_torrents()` gets extremely slow for if the number of torrent is too high
+    torrents = json.loads(tc._http_query(TORRENTS_QUERY))
+    # initialize a counter with all the possible statuses
+    status = Counter({1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
+    status.update(y['status'] for y in torrents['arguments']['torrents'])
+    for status_id, status_text in STATUS.items():
+        _metric_name = f'{METRIC_PREFIX}_{status_text}'
+        _return.append((f'# TYPE {_metric_name}', 'gauge'))
+        _return.append((_metric_name, status[status_id]))
+
     # https://prometheus.io/docs/instrumenting/writing_exporters/#metrics-about-the-scrape-itself
     _metric_name = f'{METRIC_PREFIX}_scrape_duration_seconds'
     _return.append((f'# TYPE {_metric_name}', 'gauge'))
-    _return.append((_metric_name,  (datetime.datetime.now() - start).total_seconds()))
+    _return.append((_metric_name, (datetime.datetime.now() - start).total_seconds()))
 
     response = make_response('\n'.join([f'{x[0]} {x[1]}' for x in _return]), 200)
     response.mimetype = "text/plain"
